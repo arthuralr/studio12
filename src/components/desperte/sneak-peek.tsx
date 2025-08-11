c1aa27194e40039896645b8464ecf12769c5188a
Janaina Rezende <janainarezende3@gmail.com>
2025-08-11 11:22:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h2 class="font-my

--- src/components/desperte/sneak-peek.tsx
+++ src/components/desperte/sneak-peek.tsx
@@ -5,7 +5,7 @@ export function SneakPeek() {
     <section className="py-20 sm:py-28 bg-[#2B212E]">
       <div className="container mx-auto px-4 text-center">
         <h2 className="font-mystic text-4xl md:text-5xl font-bold text-[#5C4B99] mb-12">
-          Veja a beleza que te espera
+          Descubra a Beleza que te Espera
         </h2>
         <Image
           src="https://images.unsplash.com/photo-1594744803329-e58b31de8bf5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMXx8bXVsaGVyfGVufDB8fHx8MTc1NDkwMzE2MHww&ixlib=rb-4.1.0&q=80&w=1080"
