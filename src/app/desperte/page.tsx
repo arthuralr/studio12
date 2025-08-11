91e745ac9ed010bed7bdc5e4f118b724c7bd5c0f
Janaina Rezende <janainarezende3@gmail.com>
2025-08-11 11:22:05 +0000

    Adcione ao lado da imagem o texto: Sobre a Autora Paloma Martins Após gu

--- src/app/desperte/page.tsx
+++ src/app/desperte/page.tsx
@@ -7,7 +7,6 @@ import { Portals } from "@/components/desperte/portals";
 import { SneakPeek } from "@/components/desperte/sneak-peek";
 import { TheSolution } from "@/components/desperte/solution";
 import { Footer } from "@/components/desperte/footer";
-import { ImpactImage } from "@/components/desperte/impact-image";
 
 export default function DespertePage() {
   return (
@@ -20,7 +19,6 @@ export default function DespertePage() {
         <Portals />
         <SneakPeek />
         <Offer />
-        <ImpactImage />
         <Author />
       </main>
       <Footer />
