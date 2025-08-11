aeb38173c5a55a709566ac96537df01a97c295b3
Janaina Rezende <janainarezende3@gmail.com>
2025-08-11 11:33:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/conquista/layout.tsx
+++ src/app/conquista/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from 'next';
 import '../globals.css';
-import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
@@ -14,16 +13,9 @@ export default function ConquistaLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="!scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&family=Garamond:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body>
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <>
+      {children}
+      <Toaster />
+    </>
   );
 }
